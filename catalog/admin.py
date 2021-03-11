b7b1209090513b12dc7cab20805ab87f22cc3a3a
Sean Oyler <xrysen@gmail.com>
2021-03-11 13:22:33 -0800

    Updates field lists for Book instances

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,17 +9,37 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class AuthorInline(admin.TabularInline):
+  model = Author
+  admin.TabularInline.extra = 0
+
 class AuthorAdmin(admin.ModelAdmin):
   list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+  fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceInline(admin.TabularInline):
+  model = BookInstance
+  admin.TabularInline.extra = 0
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
   list_display = ('title', 'author', 'display_genre')
+  inlines = [BookInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+  list_display = ('book', 'status', 'due_back', 'id')
   list_filter = ('status', 'due_back')
+
+  fieldsets = (
+    (None, {
+      'fields': ('book', 'id')
+    }),
+    ('Availability', {
+      'fields': ('status', 'due_back')
+    })
+  )
 
 # The @admin.register does the same thing as admin.site.register(Author, AuthorAdmin)
