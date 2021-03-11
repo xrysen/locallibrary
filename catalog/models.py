b4c0d0ec0ffae9ba12f838e4785f71b1e29272cd
Sean Oyler <xrysen@gmail.com>
2021-03-11 10:27:23 -0800

    Adds language model

--- catalog/models.py
+++ catalog/models.py
@@ -7,7 +7,13 @@ class Genre(models.Model):
   def __str__(self):
     return self.name
 
-  from django.urls import reverse
+ class Language(models.Model):
+   name = models.CharField(max_length=100, help_text="Enter natural language for the book")
+
+   def __str__(self):
+     return self.name
+
+from django.urls import reverse
 
 class Book(models.Model):
   title = models.CharField(max_length=200)
@@ -15,6 +21,7 @@ class Book(models.Model):
   summary = models.TextField(max_length=1000, help_text='Enter a brief description for a book')
   isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Characters <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
   genre = models.ManyToManyField(Genre, help_text='Select a Genre for this book')
+  language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
   def __str__(self):
     return self.title
