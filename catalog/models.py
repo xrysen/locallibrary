b5094f1bd520ddca35e522532b90be6e627139b2
Sean Oyler <xrysen@gmail.com>
2021-03-11 10:24:38 -0800

    Adds author model and updates migrations

--- catalog/models.py
+++ catalog/models.py
@@ -9,18 +9,18 @@ class Genre(models.Model):
 
   from django.urls import reverse
 
-  class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, help_text='Enter a brief description for a book')
-    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Characters <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text='Select a Genre for this book')
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+  summary = models.TextField(max_length=1000, help_text='Enter a brief description for a book')
+  isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Characters <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+  genre = models.ManyToManyField(Genre, help_text='Select a Genre for this book')
 
-    def __str__(self):
-      return self.title
+  def __str__(self):
+    return self.title
 
-    def get_absolute(self):
-      return reverse('book-detail', args=[str(self.id)])
+  def get_absolute(self):
+    return reverse('book-detail', args=[str(self.id)])
 
 import uuid
 
@@ -50,3 +50,18 @@ class BookInstance(models.Model):
 
   def __str__(self):
     return f'{self.id} ({self.book_title})'
+
+class Author(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  date_of_birth = models.DateField(null=True, blank=True)
+  date_of_death = models.DateField('Died', null=True, blank=True)
+
+  class Meta:
+    ordering = ['last_name', 'first_name']
+
+  def get_absolute_url(self):
+    return reverse('author-detail', args=[str(self.id)])
+
+  def __str__(self):
+    return f'{self.last_name}, {self.first_name}'
